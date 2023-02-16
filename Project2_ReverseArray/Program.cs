9faf3e29a09c103da4ffb69c16e4b7b977b19e4c
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-16 13:23:58 +0300

    Project 2. Array reversing

---
+++ Project2_ReverseArray/Program.cs
@@ -0,0 +1,24 @@
+﻿// Перевернуть массив (было 1 2 3, стало 3 2 1)
+int [] a = { 3, 4 , 2 , 1, 5};
+int t;
+Console.WriteLine("Вывод первой половины массива");
+for (int i=0;i<a.Length/2; i++) // делим массив пополам
+{
+System.Console.WriteLine(a[i]);
+}
+Console.WriteLine("Вывод первой половины массива (с конца)");
+for (int i=0;i<a.Length/2; i++)
+{
+System.Console.WriteLine(a[a.Length-i-1]); 
+}
+for (int i=0;i<a.Length/2; i++) // переворот массива
+{
+  t=a[i];
+  a[i]=a[a.Length-i-1];
+  a[a.Length-i-1]=t;
+}
+Console.WriteLine("Результат переворота");
+for (int i=0;i<a.Length; i++) // вывод ответа
+{
+  System.Console.Write(a[i]);
+}
