dbcbc032d9e912c1275d3b0dd0c40461abcea751
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-16 12:10:53 +0300

    Project 1. How much numbers more than zero

---
+++ Project1_HowMuchMore/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 51. С клавиатуры вводится число N. Затем вводятся N чисел. 
+Определить сколько чисел больше 0 введено с клавиатуры */
+
+namespace MainSpace
+{
+  namespace UnderSpace // в прострастве имен можно создавать подпространства
+  {
+    class Training // в прострастве имен обяз. задать класс
+    {
+      static void Main() // задаем точку входа
+      {
+        System.Console.WriteLine("Введите N");
+        int N = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Вводите числа");
+        int k=0;
+        for (int i = 0; i<N; i++) // вводим числа N раз
+        {
+          int n = int.Parse(Console.ReadLine());
+          if (n>0) k++;
+        }
+        System.Console.Write("Ответ:");
+        System.Console.WriteLine(k);
+        MainSpace.UnderSpace.Training.Main(); // зацикливание программы
+      }
+    }
+  }
+}
